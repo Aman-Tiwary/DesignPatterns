4462f7e4ea0ec25efec76d84e4671ebd59866bdb
Aman Tiwary <105771487+Aman-Tiwary@users.noreply.github.com>
2023-04-10 12:03:34 +0530

    Delete MainBuilder.java

--- BuilderPattern/MainBuilder.java
+++
@@ -1,6 +0,0 @@
-public class MainBuilder {
-    public static void main(String[] args) {
-        Human human1= new Human.HumanBuilder().setAge(5).setGender("Male").setName("Aman").build();
-        human1.printHuman();
-    }
-}
